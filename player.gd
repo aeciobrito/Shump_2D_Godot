17ebd78fa179ee0106fb95d8570c3572b035eb1b
aeciobrito <aeciobrito@gmail.com>
2024-05-23 16:44:28 -0300

    Coding the Player

---
+++ player.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+@export var speed = 150
+
+@onready var screensize = get_viewport_rect().size
+
+func _process(delta):
+	var input = Input.get_vector("left","right","up","down")
+	
+	if input.x > 0:
+		$Ship.frame = 2
+		$Ship/Boosters.animation  = "right"
+	elif  input.x < 0:
+		$Ship.frame = 0
+		$Ship/Boosters.animation  = "left"
+	else:
+		$Ship.frame = 1
+		$Ship/Boosters.animation = "forward"
+		
+	position += input.normalized() * speed * delta
+	position = position.clamp(Vector2(8, 8), screensize - Vector2(8, 8))
