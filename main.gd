dbff6b451ada62428de0c63339c32a2a9f2de3a9
aeciobrito <aeciobrito@gmail.com>
2024-05-28 14:23:16 -0300

    Starting and Ending the Game

--- main.gd
+++ main.gd
@@ -2,9 +2,15 @@ extends Node2D
 
 var enemy = preload("res://enemy.tscn")
 var score = 0
+var playing = false
+
+@onready var start_button = $CanvasLayer/CenterContainer/Start
+@onready var game_over = $CanvasLayer/CenterContainer/GameOver
 
 func _ready():
-	spawn_enemies()
+	game_over.hide()
+	start_button.show()
+	#spawn_enemies()
 	
 func spawn_enemies():
 	for x in range(9):
@@ -18,3 +24,21 @@ func spawn_enemies():
 func _on_enemy_died(value):
 	score += value
 	$CanvasLayer/UI.update_score(score)
+
+
+func _on_start_pressed():
+	start_button.hide()
+	new_game()
+
+func new_game():
+	score = 0
+	$CanvasLayer/UI.update_score(score)
+	$Player.start()
+	spawn_enemies()
+
+func _on_player_died():
+	get_tree().call_group("enemies", "queue_free")
+	game_over.show()
+	await get_tree().create_timer(2).timeout
+	game_over.hide()
+	start_button.show()
